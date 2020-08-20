cf1d6267bb3e0b583d1522fbedfae9850f60eb4c
Aldungarov <kinanet@bigmir.net>
2020-08-20 20:03:52 +0300

    Third home work started.

---
+++ Aldungarov/HomeWorkNumberThree/ThirdHomeWork.BLL/Enum/Title.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdHomeWork.BLL.Enum
+{
+    public enum Title
+    {
+        Mr = 1,
+        Miss,
+        Mrs,
+        Dr
+    }
+}
---
+++ Aldungarov/HomeWorkNumberThree/ThirdHomeWork.BLL/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdHomeWork.BLL.Enum;
+
+namespace ThirdHomeWork.BLL.Models
+{
+    public class User
+    {
+        public string FirstName;
+        
+        public string LastName;
+        
+        public string Phone;
+        
+        public string Email;
+        
+        public Title Title;
+        
+        public Country Country;
+        
+        public City City;
+        
+        public string Comments;
+    }
+}
