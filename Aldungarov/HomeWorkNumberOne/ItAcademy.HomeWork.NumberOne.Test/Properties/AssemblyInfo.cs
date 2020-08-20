801953ef3dd5d93d449a3dfe3be92bcaced6bd81
Aldungarov <kinanet@bigmir.net>
2020-08-20 20:06:20 +0300

    Home work number one.

---
+++ Aldungarov/HomeWorkNumberOne/ItAcademy.HomeWork.NumberOne.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ItAcademy.HomeWork.NumberOne.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ItAcademy.HomeWork.NumberOne.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80dab805-c201-4fa1-b89d-8a43e3b17e93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
